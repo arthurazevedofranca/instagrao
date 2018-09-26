facecc6496adb10f943572cb54f51fcc99090ea9
arthur <arthurfrana@gmail.com>
2018-09-25 22:31:24 -0300

    acertando teste do getImage

--- getImage.js
+++ getImage.js
@@ -2,15 +2,13 @@
 
 const image = require('./lib/image');
 const AWS = require('./lib/aws');
-const s3 = new AWS.S3();
 
 const getImage = (s3objectkey) => {
 
     return image
         .get(s3objectkey)
-        .get('key')
-        .then((key) => {
-            return s3.getObject({ Bucket: process.env.BUCKETNAME_IMAGE, Key: key }).promise();  
+        .then((data) => {
+            return new AWS.S3().getObject({ Bucket: process.env.BUCKETNAME_IMAGE, Key: data.key }).promise();  
         })
         .then((data) => {
             return { contentType: data.ContentType, image: new Buffer(data.Body.buffer, 'base64') };
--- getImage.test.js
+++ getImage.test.js
@@ -1,16 +1,25 @@
 'use strict';
 
 let AWS = require('./lib/aws');
+let image = require('./lib/image');
 let getImage = require('./getImage');
 
+jest.mock('./lib/aws');
 
 describe('getImage', () => {
 
     test('get image with s3objectkey', () => {
-        const s3objectkey = 'images/vc-escala.jpg';
+        const s3objectkey = 'imagesvc-escala.jpg';
         process.env.BUCKETNAME_IMAGE = 'test.instagrao.media';
-        let getObject = jest.fn().mockReturnValue(Promise.resolve({ contentType: 'jpeg', Body: {buffer:''} }));
-        AWS.S3 = () => {return  { getObject: getObject }; };
+
+        let getObject = jest.fn().mockReturnValue({ promise: () => Promise.resolve({ contentType: 'jpeg', Body: {buffer:''} }) });
+        AWS.S3 = jest.fn().mockImplementation(() => {
+            return { 
+                getObject: getObject
+            };
+        });
+
+        image.get = jest.fn().mockReturnValue(Promise.resolve({ key: s3objectkey }));
 
         expect.assertions(2);
         return getImage(s3objectkey).then((data) => {
@@ -19,6 +28,7 @@ describe('getImage', () => {
                 Key: s3objectkey
             };
 
+            expect(image.get).toBeCalledWith(s3objectkey);
             expect(getObject).toBeCalledWith(paramsExpected);
         });
 
